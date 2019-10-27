8e62af096bdb3b222abe451a2f913884f09cdcb9
AndanteDevs <orrdaniel73@gmail.com>
2019-10-27 17:00:27 +0000

    Initial feature commit
    + base
    + Nether Gold Ore
    + Example Item
    * .gitignore

---
+++ src/main/java/net/pandorica/ModBlocks.java
@@ -0,0 +1,20 @@
+package net.pandorica;
+
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+    public static final Block NETHER_GOLD_ORE = new Block(FabricBlockSettings.copy(Blocks.GOLD_ORE).build());
+    
+    public static void registerAll()
+    {
+        Registry.register(Registry.BLOCK, new Identifier("pandorica", "nether_gold_ore"), NETHER_GOLD_ORE);
+        Registry.register(Registry.ITEM, new Identifier("pandorica", "nether_gold_ore"), new BlockItem(NETHER_GOLD_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+    }
+}
