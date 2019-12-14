701e685086f78f5125e5164484e5c1a3583fb979
Daniel Orr <orrdaniel73@gmail.com>
2019-12-14 21:14:31 +0000

    'fix' crash on load

--- src/main/java/net/pandorica/Pandorica.java
+++ src/main/java/net/pandorica/Pandorica.java
@@ -1,24 +1,24 @@
 package net.pandorica;
 
-import net.pandorica.entities.liquefied_skeleton.LiquefiedSkeletonEntity;
+// import net.pandorica.entities.liquefied_skeleton.LiquefiedSkeletonEntity;
 import net.pandorica.init.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityCategory;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
+// import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+// import net.minecraft.entity.EntityCategory;
+// import net.minecraft.entity.EntityDimensions;
+// import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+// import net.minecraft.util.registry.Registry;
 
 public class Pandorica implements ModInitializer {
 
     public static final String MOD_ID = "pandorica";
     public static final ItemGroup PANDORICA_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "pandorica_group"), () -> new ItemStack(PandoricaBlocks.ERUPTION_BLOCK));
 
-    public static final EntityType<LiquefiedSkeletonEntity> LIQUEFIED_SKELETON = Registry.register(Registry.ENTITY_TYPE, new Identifier(MOD_ID, "liquefied_skeleton"), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, LiquefiedSkeletonEntity::new).setImmuneToFire().size(EntityDimensions.fixed(.7F, 2.4F)).build());
+    // public static final EntityType<LiquefiedSkeletonEntity> LIQUEFIED_SKELETON = Registry.register(Registry.ENTITY_TYPE, new Identifier(MOD_ID, "liquefied_skeleton"), FabricEntityTypeBuilder.create(EntityCategory.MONSTER, LiquefiedSkeletonEntity::new).setImmuneToFire().size(EntityDimensions.fixed(.7F, 2.4F)).build());
 
     @Override
     public void onInitialize() {
--- src/main/java/net/pandorica/world/biome/SoulValleyBiome.java
+++ src/main/java/net/pandorica/world/biome/SoulValleyBiome.java
@@ -1,11 +1,11 @@
 package net.pandorica.world.biome;
 
-import net.minecraft.entity.EntityCategory;
+// import net.minecraft.entity.EntityCategory;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ProbabilityConfig;
 import net.minecraft.world.gen.carver.Carver;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
-import net.pandorica.Pandorica;
+// import net.pandorica.Pandorica;
 import net.minecraft.world.gen.GenerationStep;
 
 public class SoulValleyBiome extends Biome {
@@ -14,6 +14,6 @@ public class SoulValleyBiome extends Biome {
                 .precipitation(Biome.Precipitation.NONE).category(Biome.Category.NETHER).depth(0.1F).scale(0.2F)
                 .temperature(2.0F).downfall(0.0F).waterColor(4159204).waterFogColor(329011).parent((String) null));
         this.addCarver(GenerationStep.Carver.AIR, configureCarver(Carver.HELL_CAVE, new ProbabilityConfig(0.2F)));
-        this.addSpawn(EntityCategory.MONSTER, new Biome.SpawnEntry(Pandorica.LIQUEFIED_SKELETON, 1, 4, 4));
+        // this.addSpawn(EntityCategory.MONSTER, new Biome.SpawnEntry(Pandorica.LIQUEFIED_SKELETON, 1, 4, 4));
     }
 }
